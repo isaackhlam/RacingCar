f2a1a6fcdfb34d5f25e583ead44d867089bab7a9
Anson Cheng <Anson.12666@icloud.com>
2022-10-24 12:57:51 +0800

    build: done motor part
    Upload via green upload button

--- reciever.ino
+++ reciever.ino
@@ -1,54 +1,91 @@
-##RECIVER
-
+//RECIVER
 #include <esp_now.h>
 #include <WiFi.h>
 
-// REPLACE WITH THE MAC Address of your receiver 
-uint8_t broadcastAddress[] = {0xF0, 0x08, 0xD1, 0xC7, 0xAA, 0xF8};
+//Define Pin Port and Preference
+#define SERVO 14
+#define MOTOR_LEFT_A 26
+#define MOTOR_LEFT_B 27
+#define MOTOR_RIGHT_A 16
+#define MOTOR_RIGHT_B 17
+#define WIFI_CHANNEL 0
 
-// Variable to store if sending data was successful
-String success;
-typedef struct car_data {
-    double speed;
-    double angle;
-} car_data;
-car_data RemoteDatas;
-car_data incomingReadings;
-esp_now_peer_info_t peerInfo;
+//Set send target and recieve data structure
+uint8_t sendTargetMAC[] = {0xF0, 0x08, 0xD1, 0xC7, 0xAA, 0xF8};
+struct car_data {
+    int speed;
+    int angle;
+};
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  if (status == ESP_NOW_SEND_SUCCESS) return;
+  Serial.print("FAILED:");
+  Serial.println(WiFi.macAddress());
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-  Serial.printf("%.5f,%.5f\n", incomingReadings.speed,incomingReadings.angle);
+  //Get cardata
+  car_data Recieve_Data;
+  memcpy(&Recieve_Data, incomingData, sizeof(Recieve_Data));
+
+  //Set Direction (-1:Backwards, 0: Stop, 1: Foward)
+  //If Speed = 0, STOP, angle < 0 BACKWARDS, angle > 0, FOWARD
+  int direction = !Recieve_Data.speed ? 0 : (
+                    Recieve_Data.angle > 0 ? 1 : -1
+                  );
+  backwheel(direction, abs(Recieve_Data.speed));
+
+  Serial.printf("|%d| speed: %d, angle: %d\n", direction, Recieve_Data.speed, Recieve_Data.angle);
+}
+
+void backwheel(int direction, int speed) { 
+  //Set Direction (-1:Backwards, 0: Stop, 1: Foward)
+
+  //Motor A: Forward Speed, Motor B: Backwards Speed
+  analogWrite(MOTOR_LEFT_A, direction == 1 ? speed : 0);
+  analogWrite(MOTOR_RIGHT_A, direction == 1 ? speed : 0);
+
+  analogWrite(MOTOR_LEFT_B, direction == -1 ? speed : 0);
+  analogWrite(MOTOR_RIGHT_B, direction == -1 ? speed : 0);
 }
  
 void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
 
+  //Init Motor
+  pinMode(MOTOR_LEFT_A, OUTPUT);
+  pinMode(MOTOR_LEFT_B, OUTPUT);
+  pinMode(MOTOR_RIGHT_A, OUTPUT);
+  pinMode(MOTOR_RIGHT_B, OUTPUT);
+
+  //Init Wifi
   WiFi.mode(WIFI_STA);
+  Serial.flush();
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  esp_now_register_send_cb(OnDataSent);
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
+
+  //Create RSP Channel
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, sendTargetMAC, 6);
+  peerInfo.channel = WIFI_CHANNEL;
   peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
+
+  //Set Up Event Listener on Send and Recieve
+  esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-
- 
 void loop() {
-
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &RemoteDatas, sizeof(RemoteDatas));
-
-  delay(10);
+  //send device boot time
+  int Send_Data = millis();
+  Serial.printf("Send Time: %d\n", Send_Data);
+  esp_err_t result = esp_now_send(sendTargetMAC, (uint8_t *) &Send_Data, sizeof(Send_Data));
+  if (result != ESP_OK) Serial.println("Error sending the data");
 }
--- sender.ino
+++ sender.ino
@@ -1,70 +1,90 @@
-##SENDER
-
+//SENDER
 #include <esp_now.h>
 #include <WiFi.h>
-int VRx = 34;
-int VRy = 35;
-int SW = 33;
-int mapX = 0, mapY = 0, SW_state = 0;
-double x = 0, y = 0, speed = 0, angle = 0, defaultx = 0, defaulty = 0;
 
-// REPLACE WITH THE MAC Address of your receiver 
-uint8_t broadcastAddress[] = {0xB8, 0xD6, 0x1A, 0xB3, 0x16, 0x78};
+//Define Pin Port and Preference
+#define VRx 34
+#define VRy 35
+#define SW 33
+#define WIFI_CHANNEL 0
+#define MOTOR_MAX_SPEED 255
+#define MOTOR_MIN_SPEED 60
 
-// Variable to store if sending data was successful
-String success;
-typedef struct car_data {
-    double speed;
-    double angle;
-} car_data;
-car_data RemoteDatas;
-car_data incomingReadings;
-esp_now_peer_info_t peerInfo;
-
+//Set send target and send data structure
+uint8_t sendTargetMAC[] = { 0x10, 0x52, 0x1C, 0x5C, 0xD1, 0x74 };
+struct car_data {
+    int speed;
+    int angle;
+};
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\nLast Packet Send Status: ");
-  if(status != ESP_NOW_SEND_SUCCESS) Serial.println( "Delivery Fail");
-  success = status == 0 ? "Delivery Success :)" : "Delivery Fail :(";
+  if (status == ESP_NOW_SEND_SUCCESS) return;
+  Serial.print("FAILED:");
+  Serial.println(WiFi.macAddress());
 }
- 
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  //get reciever boot time
+  int Recieve_Data;
+  memcpy(&Recieve_Data, incomingData, sizeof(Recieve_Data));
+  Serial.printf("Recieve Time: %d\n", Recieve_Data);
+}
+
 void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
-  defaultx = analogRead(VRx);
-  defaulty = analogRead(VRy);
 
+  //Init Wifi
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  esp_now_register_send_cb(OnDataSent);
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;   
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+
+  //Create RSP Channel
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, sendTargetMAC, 6);
+  peerInfo.channel = WIFI_CHANNEL;
+  peerInfo.encrypt = false;
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
+
+  //Set Up Event Listener on Send and Recieve
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
 
- 
+
 void loop() {
+  //map read data to -MOTOR_MAX_SPEED to MOTOR_MAX_SPEED
+  double x = map(analogRead(VRx) - 1975, -1960, 2160, -MOTOR_MAX_SPEED, MOTOR_MAX_SPEED);
+  double y = map(analogRead(VRy) - 1975, -1960, 2160, -MOTOR_MAX_SPEED, MOTOR_MAX_SPEED);
 
-  x = map((int)(analogRead(VRx) - defaultx) / 32 * 32, -1900, 1900, -1000, 1000);
-  y = map((int)(analogRead(VRy) - defaulty) / 32 * 32, -1900, 1900, -1000, 1000);
-  x = abs(x) > 1000 ? x / abs(x) * 1000 : x;
-  y = abs(y) > 1000 ? y / abs(y) * 1000 : y;
-  RemoteDatas.angle = atan(y / (x == 0 ? 0.00001 : x)) * 180 / PI;
-  RemoteDatas.speed = (angle > 0 ? 1 : -1) * sqrt(x * x + y * y) / sqrt(2000000);
+  //Limit output range (<= 60 -> 0, >MOTOR_MAX_SPEED -> MOTOR_MAX_SPEED)
+  x = abs(x) < MOTOR_MIN_SPEED ? 0 : (
+        x > MOTOR_MAX_SPEED ? MOTOR_MAX_SPEED : (
+        x < -MOTOR_MAX_SPEED ? -MOTOR_MAX_SPEED : 0
+      ));
+  y = abs(y) < MOTOR_MIN_SPEED ? 0 : (
+        y > MOTOR_MAX_SPEED ? MOTOR_MAX_SPEED : (
+        y < -MOTOR_MAX_SPEED ? -MOTOR_MAX_SPEED : 0
+      ));
 
-  Serial.printf("speed: %.5f, angle: %.5f\n",RemoteDatas.angle,RemoteDatas.speed);
+  //Create output data (cal the angle & speed)
+  car_data Send_Data;
+  if(!y) { //pos on x-axis
+    Send_Data.angle = x < 0 ? 180 : 0;
+    Send_Data.speed = x;
+  } else { //to polar cood.
+    Send_Data.angle = (atan2(y, x) / PI) * 180;
+    Send_Data.speed = abs(y);
+  }
+  Serial.printf("[%.2f,%.2f] Speed: %d, Angle: %d\n",x,y,Send_Data.speed,Send_Data.angle);
 
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &RemoteDatas, sizeof(RemoteDatas));
-  if (result != ESP_OK) 
-    Serial.println("Error sending the data");
-  
-  delay(10);
+  //send Data
+  esp_err_t result = esp_now_send(sendTargetMAC, (uint8_t *)&Send_Data, sizeof(Send_Data));
+  if (result != ESP_OK) Serial.println("FAILED SEND");
 }
