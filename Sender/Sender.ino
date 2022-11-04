34394e9ae8a555f6512e97a93f7cbe45bd3656ae
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2022-11-04 11:25:42 +0800

    other: code style and remove uncessary code

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -13,38 +13,35 @@
 //Set send target and send data structure
 esp_now_peer_info_t peerInfo;
 uint8_t sendTargetMAC[] = { 0x10, 0x52, 0x1C, 0x5C, 0xD1, 0x74 };
-//uint8_t sendTargetMAC[] = {0xF0, 0x08, 0xD1, 0xC7, 0xAA, 0xF8};
-struct car_data {
+int errorCount = 0;
+
+struct carData {
   int speed;
   int angle;
 };
-int errorCount = 0;
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *macAddress, esp_now_send_status_t status) {
   if (status == ESP_NOW_SEND_SUCCESS) return;
-  Serial.print("FAILED:");
+  Serial.print("FAILED: ");
   Serial.println(WiFi.macAddress());
 }
 
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-  //get receiver boot time
-  int receiveData;
-  memcpy(&receiveData, incomingData, sizeof(receiveData));
-  Serial.printf("Receive Time: %d\n", receiveData);
-}
+//* Debug only
+// void OnDataRecv(const uint8_t *macAddress, const uint8_t *incomingData, int len) {
+//   int receiveData;
+//   memcpy(&receiveData, incomingData, sizeof(receiveData));
+//   Serial.printf("Receive Time: %d\n", receiveData);
+// }
 
 void setup() {
-  // Init Serial Monitor
   Serial.begin(115200);
 
-  //Init Wifi
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     for(;;);
   }
 
-  //Create RSP Channel
   memcpy(peerInfo.peer_addr, sendTargetMAC, 6);
   peerInfo.channel = WIFI_CHANNEL;
   peerInfo.encrypt = false;
@@ -53,19 +50,21 @@ void setup() {
     for(;;);
   }
 
-  //Set Up Event Listener on Send and Receive
+  // This function act as event listener
   esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
+  //* debug only
+  // esp_now_register_recv_cb(OnDataRecv);
 }
 
 
 
 void loop() {
-  //map read data to -MOTOR_MAX_SPEED to MOTOR_MAX_SPEED
+  // TODO: Need refactor joystick calibration and mapping
+  // map read data to -MOTOR_MAX_SPEED to MOTOR_MAX_SPEED
   double x = map(analogRead(VRx) - 1975, -1960, 2160, -MOTOR_MAX_SPEED, MOTOR_MAX_SPEED);
   double y = map(analogRead(VRy) - 1975, -1960, 2160, -MOTOR_MAX_SPEED, MOTOR_MAX_SPEED);
 
-  //Limit output range (<= 60 -> 0, >MOTOR_MAX_SPEED -> MOTOR_MAX_SPEED)
+  // Limit output range (<= 60 -> 0, >MOTOR_MAX_SPEED -> MOTOR_MAX_SPEED)
   x = abs(x) < MOTOR_MIN_SPEED ? 0 : (
         x > MOTOR_MAX_SPEED ? MOTOR_MAX_SPEED : (
         x < -MOTOR_MAX_SPEED ? -MOTOR_MAX_SPEED : x
@@ -75,15 +74,17 @@ void loop() {
         y < -MOTOR_MAX_SPEED ? -MOTOR_MAX_SPEED : y
       ));
 
-  //Create output data (cal the angle & speed)
-  car_data Send_Data;
-  Send_Data.angle = y ? atan2(y, x) * 180 / PI //pos not on x-axis
+  // Create output data (cal the angle & speed)
+  // TODO: maybe rework calculation
+  carData sendData;
+  sendData.angle = y ? atan2(y, x) * 180 / PI //pos not on x-axis
                       : (x < 0 ? 180 : 0);
-  Send_Data.speed = sqrt(x * x + y * y);
-  Serial.printf("[%.2f,%.2f] Speed: %d, Angle: %d\n", x, y, Send_Data.speed, Send_Data.angle);
+  sendData.speed = sqrt(x * x + y * y);
+  Serial.printf("[%.2f,%.2f] Speed: %d, Angle: %d\n", x, y, sendData.speed, sendData.angle);
 
   //send Data
-  esp_err_t result = esp_now_send(sendTargetMAC, (uint8_t *)&Send_Data, sizeof(Send_Data));
+  esp_err_t result = esp_now_send(sendTargetMAC, (uint8_t *)&sendData, sizeof(sendData));
+  //* error count is for Debug
   errorCount = result == ESP_OK ? 0 : errorCount + 1;
   if (!(errorCount % 100)) Serial.println(result);
 }
